ff6a723f4c55f9b4f4e75652efe3871e3a3f46f4
Jam Risser <jam@codejam.ninja>
2018-11-20 04:29:38 -0600

    Fixed win bugs

--- src/pip.js
+++ src/pip.js
@@ -1,10 +1,16 @@
 import crossSpawn from 'cross-spawn';
 import path from 'path';
+import { os } from 'js-info';
 
 export default async function pip(args) {
   if (!Array.isArray(args)) args = [args];
+  const pip = path.resolve(
+    __dirname,
+    '../env',
+    os.win ? 'Scripts/pip.exe' : 'bin/pip'
+  );
   return new Promise((resolve, reject) => {
-    const cp = crossSpawn(path.resolve(__dirname, '../env/bin/pip'), args, {
+    const cp = crossSpawn(pip, args, {
       stdio: 'inherit'
     });
     cp.on('close', resolve);
--- src/python.js
+++ src/python.js
@@ -1,10 +1,16 @@
 import crossSpawn from 'cross-spawn';
 import path from 'path';
+import { os } from 'js-info';
 
 export default async function python(args) {
   if (!Array.isArray(args)) args = [args];
+  const python = path.resolve(
+    __dirname,
+    '../env',
+    os.win ? 'Scripts/python.exe' : 'bin/python'
+  );
   return new Promise((resolve, reject) => {
-    const cp = crossSpawn(path.resolve(__dirname, '../env/bin/python'), args, {
+    const cp = crossSpawn(python, args, {
       stdio: 'inherit'
     });
     cp.on('close', resolve);
